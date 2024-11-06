977a6afb5330755bc09d099e0d42e25cf1e51f62
Philip Lindskog <philip_lindskog@hotmail.com>
2024-11-06 12:20:10 +0100

    Gjorde om testimonial delen så att den nu hämtar från api

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import testimonial1 from '../assets/images/testimonial-1.svg'
-import testimonial2 from '../assets/images/testimonial-2.svg'
+import React, { useEffect, useState } from 'react'
+import avatarGirl from '../assets/images/avatarGirl.svg'
 
 const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      
+        const res = await fetch('https://win24-assignment.azurewebsites.net/api/testimonials');
+        const data = await res.json();
+        setReviews(data);
+    };
+
+    fetchReviews();
+  }, []);
+
   return (
     <section id="testimonials">
 
@@ -10,9 +22,40 @@ const Testimonials = () => {
 
             <h2>Clients are Loving Our App</h2>
 
-            <div className="img-box">
-                <img src={testimonial1} alt="User testimonial"/>
-                <img src={testimonial2} alt="User testimonial"/>
+            <div className="review">
+
+            {reviews.map((item) => (
+
+              <div key={item.id} className='card'>
+
+                <div className='quote-icon'>
+                  <i className="fa-solid fa-quote-left"></i>
+                </div>
+
+                <div className='stars'>
+                  {Array(item.starRating).fill().map((_, i) => (
+                    <i key={`filled-${i}`} className="fa-solid fa-star"></i>
+                  ))}
+                  {Array(5 - item.starRating).fill().map((_, i) => (
+                    <i key={`empty-${i}`} className="fa-regular fa-star"></i>
+                  ))}
+                  
+                </div>
+
+                <p>{item.comment}</p>
+
+                <div className='author'>
+                  <img src={item.avatarUrl} alt="" />
+
+                  <div className='name-role'>
+                    <p className='name'>{item.author}</p>
+                    <p className='role'>{item.jobRole}</p>
+                  </div>
+                </div>
+
+              </div>
+            ))}
+              
             </div>
 
         </div>
