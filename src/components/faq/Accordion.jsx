90a5f8ab4916c49d323385c37c22409f6548d9e8
Philip Lindskog <philip_lindskog@hotmail.com>
2024-11-07 11:39:04 +0100

    gjort klart accordion så det går att öppna och stänga samt hämta information från api

--- src/components/faq/Accordion.jsx
+++ src/components/faq/Accordion.jsx
@@ -1,60 +1,44 @@
 import React, { useEffect, useState } from 'react'
 
+
 const Accordion = () => {
+
+    const [faqs, setFaqs] = useState([]);
+
+    useEffect(() => {
+        const fetchFaqs = async () => {
+            const res = await fetch('https://win24-assignment.azurewebsites.net/api/faq');
+            const data = await res.json();
+            setFaqs(data);
+        }
+
+        fetchFaqs();
+    }, [])
+
+    const [isOpen, setIsOpen] = useState(null)
+    
+    const toggleOpen = (index) => {
+        setIsOpen(isOpen === index ? null : index);
+    }
 
     return (
         <div className='faq'>
-            <div className="card-faq">
-                <h4>What formats can I download my transaction history in?</h4>
-                <button className="btn-circle btn-faq">
-                    <i className="fa-solid fa-chevron-down"></i>
-                </button>
-            </div>
-
-            <div className="card-faq open">
-                <div className='question'>
-                    <h4>Can I schedule future transfers?</h4>
-                    <button className="btn-circle btn-faq">
-                        <i className="fa-solid fa-chevron-down"></i>
-                    </button>
-                </div>
-                <div className='answer'>
-                    <div className='expandable'>
-                        <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
+        
+            {faqs.map((index) => (
+                <div key={index.id} className={`card-faq ${isOpen === index ? 'open' : ''}`}>
+                    <div className='question' onClick={() => toggleOpen(index)}>
+                        <h4>{index.title}</h4>
+                        <button className="btn-circle btn-faq">
+                            <i className="fa-solid fa-chevron-down"></i>
+                        </button>
+                    </div>
+                    <div className='answer'>
+                        <div className='expandable'>
+                            <p>{index.content}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            
-            
-
-            <div className="card-faq">
-                <h4>When can I use Banking App services?</h4>
-                <button className="btn-circle btn-faq">
-                    <i className="fa-solid fa-chevron-down"></i>
-                </button>
-                <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
-            </div>
-
-            <div className="card-faq">
-                <h4>Can I create my own password that is easy for me to remember?</h4>
-                <button className="btn-circle btn-faq">
-                    <i className="fa-solid fa-chevron-down"></i>
-                </button>
-            </div>
-
-            <div className="card-faq">
-                <h4>What happens if I forget or lose my password?</h4>
-                <button className="btn-circle btn-faq">
-                    <i className="fa-solid fa-chevron-down"></i>
-                </button>
-            </div>
-
-            <div className="card-faq">
-                <h4>Is any of my personal information stored in the App?</h4>
-                <button className="btn-circle btn-faq">
-                    <i className="fa-solid fa-chevron-down"></i>
-                </button>
-            </div>
+            ))}
         </div>
     )
 }
